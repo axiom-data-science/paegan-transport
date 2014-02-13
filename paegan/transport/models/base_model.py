53d8372a859a989d7254e4c121dcd56e8386d20f
Kyle Wilcox <kyle@axiomalaska.com>
2014-02-13 17:37:10 -0500

    Add a 'name' property to each model that just returns the class name

--- paegan/transport/models/base_model.py
+++ paegan/transport/models/base_model.py
@@ -1,3 +1,8 @@
 class BaseModel(object):
-	def move(self, **kwargs):
-		raise NotImplementedError("Must define a 'move' method on the model being called")
+
+    def move(self, **kwargs):
+        raise NotImplementedError("Must define a 'move' method on the model being called")
+
+    def get_name(self):
+        return self.__class__.__name__
+    name = property(get_name, None, None)
