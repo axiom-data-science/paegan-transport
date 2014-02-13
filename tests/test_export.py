1d1d94f6447625ebb6d5ce12576803f1b167a526
Kyle Wilcox <kyle@axiomalaska.com>
2014-02-13 10:39:20 -0500

    Add a test for the MultiPoint particle trackline exporter

--- tests/test_export.py
+++ tests/test_export.py
@@ -20,5 +20,8 @@ class ExportTest(unittest.TestCase):
     def test_h5_particle_trackline(self):
         ex.H5ParticleTracklines.export(folder=self.output_path, h5_file=os.path.normpath(os.path.join(os.path.dirname(__file__), "./resources/files/hdf5_output.h5")))
 
+    def test_h5_particle_multipoint(self):
+        ex.H5ParticleMultiPoint.export(folder=self.output_path, h5_file=os.path.normpath(os.path.join(os.path.dirname(__file__), "./resources/files/hdf5_output.h5")))
+
     def test_h5_shapefile(self):
         ex.H5GDALShapefile.export(folder=self.output_path, h5_file=os.path.normpath(os.path.join(os.path.dirname(__file__), "./resources/files/hdf5_output.h5")))
