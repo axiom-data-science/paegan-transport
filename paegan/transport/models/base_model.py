8122cc6d02f1423d18dafb94335cf5f00935cfd2
Dave Foster <dave@axiomalaska.com>
2014-09-10 13:52:23 -0400

    Fix omitted name setter

--- paegan/transport/models/base_model.py
+++ paegan/transport/models/base_model.py
@@ -3,6 +3,14 @@ class BaseModel(object):
     def move(self, **kwargs):
         raise NotImplementedError("Must define a 'move' method on the model being called")
 
-    def get_name(self):
+    @property
+    def name(self):
+        if hasattr(self, '_name') and self._name:
+            return self._name
+
         return self.__class__.__name__
-    name = property(get_name, None, None)
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
